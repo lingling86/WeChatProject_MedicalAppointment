b9b9700110f3ef63541a8bc5f58fdb288df0dba4
lingling86 <2627039229@qq.com>
2022-09-29 01:44:57 +0800

    Initial Commit

---
+++ miniprogram/pages/make_appointment/make_appointment.js
@@ -0,0 +1,80 @@
+// pages/make_appointment/make_appointment.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  goto_map:function(){
+    wx.navigateTo({
+      url: '/pages/map_route_search/map_route_search',
+    })
+  },
+
+  getInputValue(event){
+
+    console.log(event.detail.value)
+
+    this.data.inputValue = event.detail.value
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
